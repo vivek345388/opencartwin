e632424c07fe65624f4aad1d87d52e7858f2d905
vivek345388 <38831771+vivek345388@users.noreply.github.com>
2019-01-02 19:07:44 +0530

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,20 +1,17 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://13.232.228.97/opencart/admin/');
-define('HTTP_CATALOG', 'http://13.232.228.97/opencart/');
+define('HTTP_SERVER', 'https://opencartwinphp.azurewebsites.net/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://13.232.228.97/opencart/admin/');
-define('HTTPS_CATALOG', 'http://13.232.228.97/opencart/');
+define('HTTPS_SERVER', 'https://opencartwinphp.azurewebsites.net/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/html/opencart/admin/');
-define('DIR_SYSTEM', '/var/www/html/opencart/system/');
-define('DIR_IMAGE', '/var/www/html/opencart/image/');
-define('DIR_STORAGE', '/var/www/html/opencart/storage/');
-define('DIR_CATALOG', '/var/www/html/opencart/catalog/');
+define('DIR_APPLICATION', 'D:\home\site\wwwroot/catalog/');
+define('DIR_SYSTEM', 'D:\home\site\wwwroot/system/');
+define('DIR_IMAGE', 'D:\home\site\wwwroot/image/');
+define('DIR_STORAGE', 'D:\home\site\wwwroot/storage/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
-define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
 define('DIR_CACHE', DIR_STORAGE . 'cache/');
 define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
@@ -23,14 +20,11 @@ define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
 define('DIR_SESSION', DIR_STORAGE . 'session/');
 define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 
-// DB
+// DB V
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
+define('DB_HOSTNAME', 'opencartphp.mysql.database.azure.com');
+define('DB_USERNAME', 'opencartadmin@opencartphp');
 define('DB_PASSWORD', 'admin.123456');
 define('DB_DATABASE', 'opencart');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
-
-// OpenCart API
-define('OPENCART_SERVER', 'https://www.opencart.com/');
